fd7f8e60099b75b799cc940e803f10b2a90ccdaa
GowthamanG <gowthaman.gobalasingam@gmail.com>
2019-05-07 12:06:58 +0200

    New Avatar

---
+++ Assets/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Actions actions;
+    private IEnumerator coroutine;
+    void Start()
+    {
+        actions = GetComponent<Actions>();
+        actions.Stay();
+
+        coroutine = Countdown(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.hasChanged)
+        {
+            actions.Walk();
+            StartCoroutine(coroutine);
+        }
+        
+        actions.Stay();
+        
+    }
+
+
+    IEnumerator Countdown(int seconds)
+    {
+        int counter = seconds;
+
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(1);
+            counter--;
+        }
+    }
+}
---
+++ Assets/Yurowm/sciFiEngineerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sciFiEngineerMovement : MonoBehaviour
+{
+    private Actions actions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        actions = GetComponent<Actions>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.hasChanged)
+            actions.Walk();
+        else
+            actions.Stay();
+        
+        
+    }
+}
